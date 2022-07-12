efb99c01b8a25746ca7e67546daefb728a947066
MoizesFSantos <moizesferreira66@hotmail.com>
2022-07-12 20:16:46 -0300

    feat: develop branch

---
+++ lib/app.dart
@@ -0,0 +1,17 @@
+import 'package:checkitout/screen/homePage.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
