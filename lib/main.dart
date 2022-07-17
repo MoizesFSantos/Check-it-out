f00b825aa2182dca92c3e639331b105b193abec9
MoizesFSantos <moizesferreira66@hotmail.com>
2022-07-17 16:28:31 -0300

    feat: started pages development

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
 import 'package:checkitout/app.dart';
+import 'package:checkitout/repositories/categories_repository.dart';
+import 'package:checkitout/repositories/task_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const App());
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider(create: (context) => TaskRepository()),
+      ChangeNotifierProvider(create: (context) => CategoryRepository()),
+    ],
+    child: const App(),
+  ));
 }
--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,10 +1,53 @@
+import 'package:checkitout/screens/categoriesPage.dart';
+import 'package:checkitout/screens/taskPage.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int initialPage = 0;
+  late PageController pc;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    pc = PageController(initialPage: initialPage);
+  }
+
+  setPage(page) {
+    setState(() {
+      initialPage = page;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: PageView(
+        controller: pc,
+        children: [
+          TaskScreen(),
+          CategScreen(),
+        ],
+        onPageChanged: setPage,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: initialPage,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.task_alt), label: 'Tasks'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), label: 'Categories'),
+        ],
+        onTap: (page) {
+          pc.animateToPage(page,
+              duration: Duration(milliseconds: 400), curve: Curves.decelerate);
+        },
+      ),
+    );
   }
 }
