21f00c27c1da0f3e9dd41aeaae34916b4c5e4636
MoizesFSantos <moizesferreira66@hotmail.com>
2022-07-21 20:34:25 -0300

    fix: changed taskRepository.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
 import 'package:checkitout/app.dart';
+import 'package:checkitout/providers.dart';
 //import 'package:checkitout/repositories/categories_repository.dart';
-import 'package:checkitout/repositories/task_repository.dart';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
   runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (context) => TaskRepository()),
-      //ChangeNotifierProvider(create: (context) => CategoryRepository()),
-    ],
+    providers: providers,
     child: const App(),
   ));
 }
--- lib/repositories/task_repository.dart
+++ lib/repositories/task_repository.dart
@@ -1,22 +1,40 @@
+import 'package:checkitout/database/databse.dart';
+import 'package:checkitout/database/objectbox.g.dart';
 import 'package:checkitout/models/task.model.dart';
 import 'package:flutter/material.dart';
 
 class TaskRepository extends ChangeNotifier {
   List<TaskModel> _tasks = [];
   List<TaskModel> _swaptasks = [];
+  late final DataBase _dataBase;
 
   List<TaskModel> get tasks => _tasks;
 
-  TaskRepository();
+  TaskRepository(this._dataBase);
+
+  Future<Box> getBox() async {
+    final store = await _dataBase.getStore();
+    return store.box<TaskModel>();
+  }
 
   save(String task) async {
     final todo = TaskModel(title: task, done: false);
+    final box = await getBox();
+    box.put(todo);
     tasks.add(todo);
     notifyListeners();
   }
 
   getAll() async {
-    _tasks = _swaptasks;
+    final box = await getBox();
+    _tasks = box.getAll() as List<TaskModel>;
+    notifyListeners();
+  }
+
+  remove(TaskModel task) async {
+    final box = await getBox();
+    box.remove(task.id);
+    tasks.remove(task);
     notifyListeners();
   }
 
